d3b257e43e5e03d8ed90ef4e2fee491806c3f3c6
KaiserBh <rezamuhamad1@gmail.com>
2021-07-05 22:56:08 +1000

    unlucky

--- bot/helper.go
+++ bot/helper.go
@@ -346,14 +346,14 @@ func getCpuUsage() (string, error) {
 	}
 
 	cpuStatSystemFresh := stat.CPUStatAll.System
-	time.Sleep(1 * time.Second)
+	time.Sleep(3 * time.Second)
 	secondRead, err := linuxproc.ReadStat("/proc/stat")
 	if err != nil {
 		log.Error("Failed to read stat possibly due not finding /proc/stat: ", err)
 		return "", err
 	}
 	cpuStatSystemNotFresh := secondRead.CPUStatAll.System
-	difference := (float64(cpuStatSystemFresh) - float64(cpuStatSystemNotFresh)) * 100 / float64(1*time.Second)
+	difference := (float64(cpuStatSystemFresh) - float64(cpuStatSystemNotFresh)) * 100 / float64(3*time.Second)
 
 	fmt.Println("cpuStatSystemFresh:", cpuStatSystemFresh)
 	fmt.Println("cpuStatSystemNotFresh:", cpuStatSystemNotFresh)
