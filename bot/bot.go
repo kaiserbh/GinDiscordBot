0afd75aacd90bb82d999a637aa2aa6d31df6df7f
KaiserBh <rezamuhamad1@gmail.com>
2021-07-05 21:56:09 +1000

    added concurrency to stats

--- bot/bot.go
+++ bot/bot.go
@@ -61,7 +61,7 @@ func Start() {
 	// generalCommands
 	go goBot.AddHandler(helpMessageHandler)
 	goBot.AddHandler(pingMessageHandler)
-	goBot.AddHandler(stats)
+	go goBot.AddHandler(stats)
 	goBot.AddHandler(setNick)
 	goBot.AddHandler(resetNickHandler)
 	//TODO:Invite get invite link for the bot
