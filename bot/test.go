98c5a3ef60de8190474f834e9c921c696bb4c158
Alexander Ng <alexng353@gmail.com>
2021-07-09 23:16:55 -0700

    match all paaaaaaaaaaaayyyyypaaallll

--- bot/test.go
+++ bot/test.go
@@ -1,6 +1,7 @@
 package bot
 
 import (
+	"bytes"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
@@ -8,17 +9,43 @@ import (
 	"github.com/mtibben/confusables"
 )
 
+var badWords = []string{"paypal", "niger"}
+
+func removeDups(s string) string {
+	var buf bytes.Buffer
+	var last rune
+	for i, r := range s {
+		if r != last || i == 0 {
+			buf.WriteRune(r)
+			last = r
+		}
+	}
+	return buf.String()
+}
+
 func chatFilter(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	// if confusables.Confusable(m.Content, "paypal") {
-	// 	s.ChannelMessageSend(m.ChannelID, "banned word")
-	// }
+	cleaned := removeDups(strings.ReplaceAll(confusables.Skeleton(m.Content), " ", ""))
 
-	if strings.Contains(m.Content, confusables.Skeleton("paypal")) {
-		s.ChannelMessageSend(m.ChannelID, confusables.Skeleton("paypal"))
+	for _, filter := range badWords {
+		if strings.Contains(cleaned, filter) {
+			s.ChannelMessageSend(m.ChannelID, "banned word")
+			return
+		}
 	}
 }
+
+// func testCmd(ctx *framework.CommandContext) {
+// 	cleaned := removeDups(strings.ReplaceAll(confusables.Skeleton(ctx.Message.Content), " ", ""))
+
+// 	for _, filter := range badWords {
+// 		if strings.Contains(cleaned, filter) {
+// 			ctx.Bot.ChannelMessageSend(ctx.Message.ChannelID, fmt.Sprintf("bad word %s detected", filter))
+// 			return
+// 		}
+// 	}
+// }
